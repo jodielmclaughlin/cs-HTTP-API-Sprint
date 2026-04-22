e08aded285aa20b13ccf89679c296fdd746a86d5
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-04-22 16:50:52 +0100

    updated with different key and id in hidden file

--- SchipolAirport/Program.cs
+++ SchipolAirport/Program.cs
@@ -5,7 +5,7 @@ namespace SchipolAirport
     {
         static async Task Main(string[] args)
         {
-            SchipolDestinationResponse? response = await SchipolAPI.GetDestinations(Utils.ID, Utils.KEY);
+            SchipolDestinationResponse? response = await SchipolAPI.GetDestinations(Utils.ID, Utils.Key);
 
             foreach (var d in response.Destination)
             {
